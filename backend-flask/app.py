717d91de1873fc7ac56bb096792ae828ea23b3f8
NasserKremid kremid <abdunnasser.kremid@hud.gov>
2024-01-04 14:52:17 +0000

    update docker files

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -65,11 +65,6 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
-@app.route("/api/activities/notifications", methods=['GET'])
-def data_notifications():
-  data = NotificationsActivities.run()
-  return data, 200
-
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):
   model = UserActivities.run(handle)
